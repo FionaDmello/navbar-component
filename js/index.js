36901fb8a55b97ddd781be9a9f25e94a77e990ac
FionaDmello <40391218+FionaDmello@users.noreply.github.com>
2024-05-15 14:44:37 +0200

    basic js functionality to toggle menu states added

--- js/index.js
+++ js/index.js
@@ -1,2 +1,15 @@
-// Write custom JavaScript here.
-// You may ignore this file and delete if if JavaScript is not required for your challenge.
+const menuButton = document.getElementById("menu-open");
+const closeButton = document.getElementById("menu-close");
+
+const handleMenuClick = () => {
+  menuButton.classList.add("hidden");
+  closeButton.classList.remove("hidden");
+};
+
+const handleCloseClick = () => {
+  menuButton.classList.remove("hidden");
+  closeButton.classList.add("hidden");
+};
+
+menuButton.addEventListener("click", handleMenuClick);
+closeButton.addEventListener("click", handleCloseClick);
