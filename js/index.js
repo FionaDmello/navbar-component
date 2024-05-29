1747f3477ae4f0828167bd9c9d80fd3473ce39ec
FionaDmello <40391218+FionaDmello@users.noreply.github.com>
2024-05-29 15:35:31 +0200

    more clean on dialog close in md to sm version of the app

--- js/index.js
+++ js/index.js
@@ -46,6 +46,42 @@ const handleMenuClick = () => {
 const handleCloseClick = () => {
   menuButton.classList.remove("hidden");
   closeButton.classList.add("hidden");
+
+  bodyEl.classList.add("pt-4", "bg-gradient-to-br", "px-4");
+
+  navEl.classList.add("pt-4");
+  navEl.classList.remove("flex", "flex-col", "gap-6", "px-4", "pt-8", "pb-4");
+
+  logoIconContainer.classList.add("py-3");
+
+  linksContainer.classList.add("hidden", "self-center");
+  linksContainer.classList.remove("flex", "flex-col", "gap-2", "grow");
+  Object.values(linksContainer.getElementsByTagName("button")).forEach(
+    (button) => {
+      button.classList.add(
+        "focus:ring",
+        "focus:ring-4",
+        "focus:ring-indigo-200"
+      );
+      button.classList.remove(
+        "gap-3",
+        "px-3",
+        "py-2",
+        "text-left",
+        "hover:bg-neutral-50",
+        "active:bg-neutral-50",
+        "active:text-neutral-900"
+      );
+    }
+  );
+
+  actionsContainer.classList.add("hidden", "self-center");
+  actionsContainer.classList.remove(
+    "flex",
+    "flex-col",
+    "gap-4",
+    "content-left"
+  );
 };
 
 menuButton.addEventListener("click", handleMenuClick);
